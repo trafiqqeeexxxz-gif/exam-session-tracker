b7beb2d16bfefaca14bb46a9b2b80475f9d1984b
trafiqqeeexxxz-gif <trafiqqeeexxxz@gmail.com>
2025-12-03 22:54:47 +0300

    "Добавлен вывод общей информации о задании"

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -6,5 +6,6 @@ int main() {
     cout << "Exam Session Results" << endl;
     cout << "Variant: Session Results" << endl;
     cout << "Author: Krivonosog Gleb" << endl;
+    cout << "Group: 25Pinj1D" << endl;
     return 0;
 }
