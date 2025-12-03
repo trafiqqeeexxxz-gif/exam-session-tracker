5bd5ed6701d255e9eef423e9ee054721b241b30d
Ivanov Egor <max203383@gmail.com>
2025-12-03 23:17:49 +0300

    Реализовано форматирование вывода в консоль

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "file_reader.h"
+#include <iomanip>
 
 using namespace std;
+
+void printSession(const StudentSession& session) {
+    cout << session.lastName << " " << session.firstName << " " << session.middleName << " | "
+        << setw(2) << setfill('0') << session.examDate.day << "."
+        << setw(2) << setfill('0') << session.examDate.month << "."
+        << setw(2) << setfill('0') << session.examDate.year << " | "
+        << "Grade: " << session.grade << " | "
+        << "Discipline: " << session.discipline
+        << endl;
+}
 
 int main() {
     cout << "Exam Session Results" << endl;
@@ -11,6 +22,10 @@ int main() {
 
     auto sessions = readDataFromFile("data.txt");
     cout << "\nRecords loaded: " << sessions.size() << endl;
+
+    for (const auto& session : sessions) {
+        printSession(session);
+    }
 
     return 0;
 }
