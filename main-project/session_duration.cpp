73e0624d86509ea0315f7a7c598fb6cf8b7b6d87
Ivanov Egor <max203383@gmail.com>
2025-12-08 00:14:20 +0300

    Добавлена заглушка функции calculateSessionDuration

---
+++ main-project/session_duration.cpp
@@ -0,0 +1,5 @@
+#include "session_duration.h"
+
+int calculateSessionDuration(const vector<StudentSession>& sessions) {
+    return 0; 
+}
---
+++ main-project/session_duration.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+#include "student_session.h"
+
+using namespace std;
+
+int calculateSessionDuration(const vector<StudentSession>& sessions);
