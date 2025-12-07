87d1c82dd202b699cc61c2fee4169612e31e1915
Ivanov Egor <max203383@gmail.com>
2025-12-08 00:43:52 +0300

    Реализована функция calculateSessionDuration

--- main-project/session_duration.cpp
+++ main-project/session_duration.cpp
@@ -1,5 +1,30 @@
 #include "session_duration.h"
+#include <algorithm>
+
+
+int dateToDays(const Date& date) {
+    return date.year * 365 + date.month * 31 + date.day;
+}
 
 int calculateSessionDuration(const vector<StudentSession>& sessions) {
-    return 0; 
+    if (sessions.empty()) {
+        return 0;
+    }
+
+    int minDays = dateToDays(sessions[0].examDate);
+    int maxDays = minDays;
+
+    for (size_t i = 1; i < sessions.size(); ++i) {
+        int currentDays = dateToDays(sessions[i].examDate);
+
+        if (currentDays < minDays) {
+            minDays = currentDays;
+        }
+
+        if (currentDays > maxDays) {
+            maxDays = currentDays;
+        }
+    }
+
+    return maxDays - minDays;
 }
